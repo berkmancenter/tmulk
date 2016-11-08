4100b9cbc34d125720753a2cbee966c75a7b9474
Ryan Morrison~Westphal <ryan@trippingthebits.com>
2016-11-07 19:22:02 -0500

    ready for multiple loops

--- tmulk.js
+++ tmulk.js
@@ -8,7 +8,8 @@ const pkg = require( path.join( __dirname, 'package.json' ) );
 const program = require( 'commander' );
 
 const Twitter = require( 'twitter' );
-let twitterCreds = require( './twitter.json' );
+const twitterCreds = require( './twitter.json' );
+const tweetAttributes = [ 'id', 'text', 'source', 'retweet_count', 'favorite_count' ];
 
 program
   .version( pkg.version )
@@ -22,13 +23,31 @@ if ( !program.args.length ) {
 let client = new Twitter( twitterCreds );
 
 for ( var handle of program.args ) {
-  console.log( `now downloading ${ handle }` );
+  //console.log( `now downloading ${ handle }` );
+  let timelineReq = {
+    screen_name: handle,
+    count: 2
+  };
 
-  client.get( 'statuses/user_timeline', {
-    screen_name: handle
-  }, function( error, tweets, response ) {
+  client.get( 'statuses/user_timeline', timelineReq, function( error, tweets, response ) {
     if ( !error ) {
-      console.log( tweets );
+      //console.log( JSON.stringify( tweets.map( function( t ) {
+      console.log( tweets.map( function( t ) {
+        return {
+          created_at: t.created_at,
+          id: t.id,
+          text: t.text,
+          source: t.source,
+          retweet_count: t.retweet_count,
+          favorite_count: t.favorite_count
+        }
+      } ) );
+      //} ) ) );
+
+      if ( tweets.length === 2 ) {
+        console.log( 'There are more' );
+      }
+
     } else {
       console.log( error );
     }
